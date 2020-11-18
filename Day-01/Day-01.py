9aadf96388616966db05097ae62806cad6d23d56
Shazid Hasan Riam <shazid.riam@northsouth.edu>
2020-11-18 21:21:51 +0600

    Add files via upload

---
+++ Day-01/Day-01.py
@@ -0,0 +1,58 @@
+#practice
+print("Hello World!")
+
+#Exercise
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print?')")
+
+#practice
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" + "Angela")
+print("Hello" + " " + "Angela")
+
+#Exercise
+print("Day 1 - String Manipulation")
+print('String manipulation is done with the "+" Sign.')
+print('e.g. print("Hello" + "World!")')
+print("New lines can be created with a backslash and n.")
+
+#Practice
+#input() will get user input in console
+#Then print() will print the word "Hello" and the user input
+# print("Hello " + input("What is your name?"))
+
+#Exercise
+# print(len(input("What is your name? ")))
+
+#Practice
+name = "Jack"
+print(name)
+
+name = "Angela"
+print (name)
+
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+#Exercise
+# a = input("a: ")
+# b = input("b: ")
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b: " + b)
+
+#Practice
+#naming rules
+
+#Project Band Name Generator
+print("Welcome to the band name generator")
+city = input("Which city did you grow up in?\n ")
+# print(city)
+pet = input("What is the name of your pet?\n ")
+print("Your band name could be "+ city + " " + pet)
